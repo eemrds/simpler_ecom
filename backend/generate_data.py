50080696fc148a8a56364306836dc3dfa56e3fa6
emrds <martinerik@protonmail.com>
2023-08-29 03:08:48 +0200

    test

---
+++ backend/generate_data.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+
+INITIAL_DATA = [
+        {"id": "1", "name": "Laptop", "price": 1000, "amount_left": 10, "image_link": "laptop.jpg"},
+        {"id": "2", "name": "Phone", "price": 500, "amount_left": 20, "image_link": "phone.jpg"},
+        {"id": "3", "name": "TV", "price": 1500, "amount_left": 5, "image_link": "tv.jpg"},
+        {"id": "4", "name": "Headphones", "price": 100, "amount_left": 50, "image_link": "headphones.jpg"}
+    ]
+
+def generate_data():
+    client = MongoClient("mongodb://mongo:27017/")
+    db = client["simpledatabase"]
+    collection = db["products"]
+
+    if collection.count_documents({}) == 0:
+        collection.insert_many(INITIAL_DATA)
+        print("Inserted initial data into database.")
+    else:
+        print("Already Exists. Skipped Generation")
---
+++ backend/simple_api.py
@@ -0,0 +1,80 @@
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs, urlparse
+
+from generate_data import generate_data
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+generate_data()
+
+class SimpleAPI(BaseHTTPRequestHandler):
+    def query_db(self, filter={}, collection="products"):
+        client = MongoClient("mongodb://mongo:27017/")
+        db = client.simpledatabase
+        coll = db[collection]
+        return list(coll.find(filter))
+
+    def do_GET(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        filter = {}
+
+        if "id" in query_components:
+            product_id = query_components["id"][0]
+            filter = {'id': product_id}
+            data_from_mongo = self.query_db(filter)
+
+            if isinstance(data_from_mongo, dict):
+                data_from_mongo["_id"] = str(data_from_mongo["_id"])
+        else:
+            data_from_mongo = self.query_db(filter)
+            for item in data_from_mongo:
+                if "_id" in item:
+                    item["_id"] = str(item["_id"])
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        response = {
+            "message": "Data fetched",
+            "data": data_from_mongo
+        }
+        self.wfile.write(json.dumps(response, default=str).encode("utf-8"))
+
+    def do_PUT(self):
+        length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(length)
+        post_data = json.loads(post_data.decode('utf-8'))
+        
+        # Check for required fields
+        if "id" not in post_data:
+            self.send_response(400)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            response = {"message": "Missing 'id'"}
+            self.wfile.write(json.dumps(response).encode("utf-8"))
+            return
+
+        product_id = post_data['id']
+
+        # Connect to MongoDB
+        client = MongoClient("mongodb://mongo:27017/")
+        db = client.simpledatabase
+        coll = db["products"]
+
+        # Update stock
+        coll.update_one({'id': product_id}, {'$inc': {'amount_left': -1}})
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        response = {"message": "Stock decreased by 1"}
+        self.wfile.write(json.dumps(response).encode("utf-8"))
+
+if __name__ == "__main__":
+    server_address = ("", 8080)
+    httpd = HTTPServer(server_address, SimpleAPI)
+    print("Running server...")
+    httpd.serve_forever()
