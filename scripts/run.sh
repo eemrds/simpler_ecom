c5dca0bb2797e81d189ab89873eb95fdf616502e
emrds <martinerik@protonmail.com>
2023-10-11 17:29:45 +0200

    merging scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,10 +6,10 @@ read -p "Enter IP address: " ip
 ip=${ip:-"127.0.0.1"}
 
 set -x
-./scripts/install.sh
+sudo ./scripts/install.sh
 echo "API_IP=${ip}" > ./frontend/.env
-./scripts/dockerhub_push.sh
-./scripts/kubernetes_launch.sh
+sudo ./scripts/dockerhub_push.sh
+sudo ./scripts/kubernetes_launch.sh
 set +x
 
 echo "Kubernetes cluster launched. Waiting for pods to be ready..."
